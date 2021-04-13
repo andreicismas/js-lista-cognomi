8f83985d4d014eb009e76c9b181757d246383cfc
andreicismas <andrei_cismas@yahoo.it>
2021-04-13 22:46:20 +0200

    da finire le condizioni

---
+++ script.js
@@ -0,0 +1,28 @@
+// la mia lista di cognomi 
+var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+
+// inserisci il tuo cognome
+var cognomiDaRegistrare = prompt("inserisci il tuo cognome");
+
+
+
+
+// i nuovi cognomi pushati in "la mia lista di cognomi "
+cognomi.push(cognomiDaRegistrare);
+
+// la lista in ordine alfabetica
+cognomi.sort();
+
+// metoto for /per stampare in html /e stampare la posizione del nuovo cognome inserito 
+
+var whileIndex=0;
+
+while (whileIndex < cognomi.length) {
+
+        document.getElementById("lista_cog").innerHTML += "<li>" + cognomi[whileIndex] + "</li>";
+        whileIndex++
+} 
+
+var posizione = cognomi.indexOf(cognomiDaRegistrare) + 1;
+document.getElementById("position").innerHTML = "Sei nella posizione numero " + posizione + " dell'elenco ";
